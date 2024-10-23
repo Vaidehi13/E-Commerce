6894ebb194565e7b2daf8b7131066178fc3174dc
Vaidehi13 <64651061+Vaidehi13@users.noreply.github.com>
2024-10-23 17:42:39 +0530

    Created Order Service
    MySQL db
    Flyway migration
    Place order

---
+++ order-service/src/main/java/com/ecommerce/service/order/order_service/entity/Order.java
@@ -0,0 +1,23 @@
+package com.ecommerce.service.order.order_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ order-service/src/main/java/com/ecommerce/service/order/order_service/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.ecommerce.service.order.order_service.service;
+
+import com.ecommerce.service.order.order_service.dto.OrderRequest;
+import com.ecommerce.service.order.order_service.entity.Order;
+import com.ecommerce.service.order.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+    }
+}
